5c753f5e915f99e5a156dc798f6a164fada6a695
nuviloz <112016742+nuviloz@users.noreply.github.com>
2022-11-30 16:40:32 -0300

    Add files via upload

---
+++ nueva/mochilas.php
@@ -0,0 +1,119 @@
+<?php
+
+require_once 'header.php';
+require_once 'menu.php';
+?>
+
+
+
+
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-md-12">            
+           <h1>Coleção 2023</h1>   
+
+            <div class="col-md-3">
+            <div class="card" style="width: 18rem;">
+             <img class="card-img-top" src="img/MOchila Ejecutiva.webp" alt="img">
+             <div class="card-body">
+             <h2> class="card-text"> Mochila Sport1.</h2>
+             <h3>R$ 150,00</h3>  
+             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="Mochila Sport1.webp">
+                   Launch demo modal
+              </button>
+    </div> 
+            </div>
+
+            <div class="col-md-3">
+            <div class="card" style="width: 18rem;">
+             <img class="card-img-top" src="img/Mochila Frontal.png" alt="Imagem de capa do card">
+             <div class="card-body">
+             <h2> class="card-text"> Mochila Paloma.</h2>
+             <h3>R$ 150,00</h3>   
+            </div>
+
+            <div class="col-md-3">
+            <div class="card" style="width: 18rem;">
+             <img class="card-img-top" src="img/Mochila masculina.png">
+             <div class="card-body">
+             <h2> class="card-text"> Mochila Paloma.</h2>
+             <h3>R$ 150,00</h3>   
+            </div>
+
+            <div class="col-md-3">
+            <div class="card" style="width: 18rem;">
+             <img class="card-img-top" src="img/Mochila Paloma.png" alt="Imagem de capa do card">
+             <div class="card-body">
+             <h2> class="card-text"> Mochila Paloma.</h2>
+             <h3>R$ 150,00</h3>   
+            </div>
+
+        </div>
+    </div>
+    <!-- Button trigger modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="MOchila Ejecutiva.png" tabindex="-1" role="dialog"
+  aria-labelledby="exampleModalLabel" aria-haidden="true">
+   <div class="modal-dialog"> role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">MOchila Ejecutiva.png</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="fechar">
+         <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+      <img class="ing-fluid" src="img/MOchila Ejecutiva.png">
+      <p> Mochila en couro legitimo, com costuras reforçadas e alcas anatomicas.
+      </div>
+
+      <div class="modal fade" id="MOchila Ejecutiva.png" tabindex="-1" role="dialog"
+  aria-labelledby="exampleModalLabel" aria-haidden="true">
+   <div class="modal-dialog"> role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">MOchila Ejecutiva.png</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="fechar">
+         <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      </div>
+
+      <div class="modal fade" id="MOchila Ejecutiva.png" tabindex="-1" role="dialog"
+  aria-labelledby="exampleModalLabel" aria-haidden="true">
+   <div class="modal-dialog"> role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">MOchila Ejecutiva.png</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="fechar">
+         <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+      </div>
+
+      <div class="modal fade" id="MOchila Ejecutiva.png" tabindex="-1" role="dialog"
+  aria-labelledby="exampleModalLabel" aria-haidden="true">
+   <div class="modal-dialog"> role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">MOchila Ejecutiva.png</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="fechar">
+         <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      </div>
+     
+    </div>
+  </div>
+</div>
+
+     <?php
+
+require_once 'footer.php';
+?>
---
+++ nueva/roupa.php
@@ -0,0 +1,98 @@
+<<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+    <h1>Roupas</h1>
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-md-12">            
+                <img src="img/roupa deportiva 2.jpg" class="img-fluid">
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-fluid texto">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h2>Conheça Algumas de Nossas exposição de roupas esportivas</h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid imagens">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="img/images.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-md-4">
+                <img src="img/Roupa dep 3.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-md-4">
+                <img src="img/Roupa dep 4.jpg" class="img-fluid">
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-fluid text-center conteudo">
+      <div class="row">
+
+          <div class="col-md-6 col-sm">
+            <img src="img/Roupa hombre 3.jpg" class="img-fluid">
+          </div>
+
+          <div class="col-md-6 col-sm">
+            <h3>Roupas Esportivas</h3></p>
+                <p> La ropa de deporte o ropa deportiva es ropa específicamente creada para la práctica de deporte,
+                     tanto por los materiales y tecnologías empleadas como por el diseño, proporcionando al deportista 
+                     comodidad, seguridad y un mayor rendimiento</p>
+          </div>
+      </div>
+    </div>
+
+    <div class="container-fluid text-center conteudo">
+      <div class="row">
+        <div class="col-md-6 col-sm">
+          <h3>Nossas Tendências</h3>
+          <p>Se espera que Sports Apparel Market impulse la creciente participación de las mujeres en actividades físicas y
+             deportivas profesionales y domésticas.</p>
+        </div>
+        <div class="col-md-6 col-sm">
+          <img src="img/Roupas Hombre..jpg" class="img-fluid">
+          
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid text-center conteudo">
+      <div class="row">
+        <div class="col-md-6 col-sm">
+          <img src="img/Roupa hombre 5.jpg" class="img-fluid">
+        </div>
+        <div class="col-md-6 col-sm">
+          <h3>Curiosidade</h3>
+          <p>Según un estudio de la consultora Global Data, para 2025 el mercado mundial de ropa deportiva crecerá un 25% y llegará a un volumen de negocios de USD 551.000 millones. Este significativo 
+            aumento se debe a que este tipo de prendas son cada vez más demandas por la tendencia a usar ropa cómoda.</p>
+        </div>
+      </div>
+
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
